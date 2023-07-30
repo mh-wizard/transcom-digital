56c8046a167556e5ab93909785e58b29a67c32b6
im-mahadi <me.mahadi@hotmail.com>
2023-07-30 23:13:56 +0600

    update middle config

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,6 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
-export const config = { matcher: '/admin/:path*' }
-
 export function middleware(request: NextRequest) {
   let cookie = request.cookies.get('admin-key')
 
